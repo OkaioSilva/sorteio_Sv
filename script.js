58b794a9fec16e484939ed3a502c6fc50bcdf0e5
Kaio <silv4kaio@hotmail.com>
2024-07-01 21:53:00 -0300

    Mudei a parte responsiva

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function sorteando(){
-    const nomes = ['Sabrina', 'Carol', 'Goiabinha', 'Stefane'];
-    const sort = Math.floor(Math.random()*nomes.length);
-    let res = document.querySelector('#res');
+(function(){
+    let res = document.querySelector('.res');
+    let botao = document.getElementById('sortear');
+    botao.addEventListener("click",sorteando);
 
-    res.innerHTML = `A sorteada foi:  ${nomes[sort]}` 
-}
 
-let botao = document.getElementById('sortear');
-botao.addEventListener("click",sorteando);
-/* Sabrina
-Carol
-Goiabinha  */
+    function sorteando(){
+        const nomes = ['Sabrina', 'Carol', 'Goiabinha', 'Stefane'];
+        const sort = Math.floor(Math.random()*nomes.length);
+        res.innerHTML = "Sorteando..."
+        setTimeout(function(el){
+            const bg = document.querySelector('.bg')
+            const container = document.querySelector('.container ul')
+            bg.style.backgroundImage = 'url(img/sabio.jpg)'
+            bg.style.opacity = '1'
+            res.innerHTML = `A sorteada foi:  ${nomes[sort]}` 
+        }, 2000)
+    
+    }
+    
+    /* Sabrina
+    Carol
+    Goiabinha  */
+
+
+})()
